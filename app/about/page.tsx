295d0bcb23fc116d5cd73d6f797552f9266d05bd
Stanislau Marudau <marudov.stas@gmail.com>
2024-08-27 22:24:39 +0300

    Implement common layout

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,9 +1,6 @@
-import Navigation from '../components/Navigation';
-
 export default function Page() {
   return (
     <div>
-      <Navigation />
       <h2>Aboout page</h2>
     </div>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,7 @@
-import Link from 'next/link';
-import Navigation from './components/Navigation';
-
 export default function Page() {
   return (
     <div>
-      <Navigation />
-      <h1>The Wild Oasis. Explore the luxury cabins§</h1>
+      <h1>The Wild Oasis. Explore the luxury cabins</h1>
     </div>
   );
 }
