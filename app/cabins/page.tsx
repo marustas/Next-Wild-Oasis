d7a2fdc677ac36f0f0f20fb64b2b339d2bb6ae35
Stanislau Marudau <marudov.stas@gmail.com>
2024-08-28 11:51:40 +0300

    Implement server-side loader

--- app/cabins/page.tsx
+++ app/cabins/page.tsx
@@ -1,7 +1,18 @@
-export default function Page() {
+import Counter from '../components/Counter';
+
+export default async function Page() {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users');
+  const data = await res.json();
+
   return (
     <div>
       <h2>Cabins page</h2>
+      <ul>
+        {data.map((e: any) => (
+          <li key={e.id}>{e.name}</li>
+        ))}
+      </ul>
+      <Counter users={data} />
     </div>
   );
 }
