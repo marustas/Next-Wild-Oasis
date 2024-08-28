f4b4c4a07b864808d80861447c5b24eec8b97745
Stanislau Marudau <marudov.stas@gmail.com>
2024-08-28 13:21:15 +0300

    feat: implement fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,10 @@
 import Logo from './_components/Logo';
 import Navigation from './_components/Navigation';
+
+import { Josefin_Sans } from 'next/font/google';
 import '@/app/_styles/globals.css';
+
+const josefin = Josefin_Sans({ subsets: ['latin'], display: 'swap' });
 
 export const metadata = {
   title: {
@@ -14,7 +18,7 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className="bg-primary-950 text-primary-100 min-h-screen">
+      <body className={`${josefin.className} bg-primary-950 text-primary-100 min-h-screen`}>
         <header>
           <Logo />
         </header>
