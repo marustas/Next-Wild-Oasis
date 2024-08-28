2b79189243fc0ff056048f32cb7ce352b7b2dc5d
Stanislau Marudau <marudov.stas@gmail.com>
2024-08-28 13:15:46 +0300

    feat: implement page metadata and favicon

--- app/cabins/page.tsx
+++ app/cabins/page.tsx
@@ -1,18 +1,11 @@
-import Counter from '../components/Counter';
+export const metadata = {
+  title: 'Cabins',
+};
 
-export default async function Page() {
-  const res = await fetch('https://jsonplaceholder.typicode.com/users');
-  const data = await res.json();
-
+export default function Page() {
   return (
     <div>
       <h2>Cabins page</h2>
-      <ul>
-        {data.map((e: any) => (
-          <li key={e.id}>{e.name}</li>
-        ))}
-      </ul>
-      <Counter users={data} />
     </div>
   );
 }
--- app/components/Counter.tsx
+++
@@ -1,9 +0,0 @@
-'use client';
-
-import { useState } from 'react';
-
-export default function Counter({ users }: any) {
-  const [count, setCount] = useState(0);
-  console.log(users);
-  return <button onClick={() => setCount((c) => c + 1)}>{count}</button>;
-}
